51a54dd7dc65d900d5a1abd5824320f9aedbd8b3
James Rose <james.rose@melbourne.co.uk>
2012-03-08 12:21:59 +0000

    Tomdoc.

--- lib/ultravm/client/vms.rb
+++ lib/ultravm/client/vms.rb
@@ -3,6 +3,12 @@ module UltraVM
     module VMs
       
       # Get all VMs
+      #
+      # Examples
+      #   client.vms 
+      #   # => [UltraVM::VM, UltraVM::VM, ...]
+      #
+      # Returns Array of UltraVM::VM objects.
       def vms
         get('host', :resident_VMs, uuid).collect do |vm|
           UltraVM::VM.new(self, vm)
@@ -10,6 +16,14 @@ module UltraVM
       end
       
       # Get all VMs which label matches +name.
+      #
+      # name - The String of the names of VMs to find.
+      #
+      # Examples
+      #   client.vms_by_name('some-name')
+      #   # => [UltraVM::VM, UltraVM::VM, ...]
+      #
+      # Returns array of UltraVM::VM object.
       def vms_by_name(name)
         get('VM', :by_name_label, name).collect do |vm|
           UltraVM::VM.new(self, vm)
@@ -17,6 +31,14 @@ module UltraVM
       end
       
       # Get first VM which label matches +name. Assumes unique labels.
+      #
+      # name - The String of the name of the VM to find.
+      #
+      # Examples
+      #   client.vm_by_name('some-name')
+      #   # => UltraVM::VM
+      #
+      # Returns UltraVM::VM object.
       def vm_by_name(name)
         vms = get('VM', :by_name_label, name)
         if vms.any?
@@ -27,6 +49,14 @@ module UltraVM
       end
       
       # Get a complete VM hash of attributes
+      #
+      # ref - The UUID of the VM to find.
+      #
+      # Examples
+      #   client.record_by_ref('uuid-xbc123123213123123')
+      #   # => Hash
+      # 
+      # Returns Hash of attributes of VM.
       def record_by_ref(ref)
         get('VM', :record, ref)
       end
